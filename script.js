ed2d1ad4f5a2db596ead153df2dc25afde1c0344
Anton <antonkosicyn@rambler.ru>
2022-10-16 12:28:05 +0300

    refactor:replace museum with online-zoo

--- script.js
+++ script.js
@@ -6,32 +6,32 @@ const progress = document.querySelector(".progress-bars-wrapper");
 const progressBarPercents = [97, 89, 85, 85, 80, 80, 90];
 
 window.addEventListener("scroll", () => {
-  mainFn();
+    mainFn();
 });
 
 const mainFn = () => {
-  if (window.pageYOffset >= navbarOffsetTop) {
-    navbar.classList.add("sticky");
-  } else {
-    navbar.classList.remove("sticky");
-  }
-
-  sections.forEach((section, i) => {
-    if (window.pageYOffset >= section.offsetTop - 10) {
-      navbarLinks.forEach((navbarLink) => {
-        navbarLink.classList.remove("change");
-      });
-      navbarLinks[i].classList.add("change");
+    if (window.pageYOffset >= navbarOffsetTop) {
+        navbar.classList.add("sticky");
+    } else {
+        navbar.classList.remove("sticky");
     }
-  });
 
-  if (window.pageYOffset + window.innerHeight >= progress.offsetTop) {
-    document.querySelectorAll(".progress-percent").forEach((el, i) => {
-      el.style.width = `${progressBarPercents[i]}%`;
-      el.previousElementSibling.firstElementChild.textContent =
-        progressBarPercents[i];
+    sections.forEach((section, i) => {
+        if (window.pageYOffset >= section.offsetTop - 10) {
+            navbarLinks.forEach((navbarLink) => {
+                navbarLink.classList.remove("change");
+            });
+            navbarLinks[i].classList.add("change");
+        }
     });
-  }
+
+    if (window.pageYOffset + window.innerHeight >= progress.offsetTop) {
+        document.querySelectorAll(".progress-percent").forEach((el, i) => {
+            el.style.width = `${progressBarPercents[i]}%`;
+            el.previousElementSibling.firstElementChild.textContent =
+                progressBarPercents[i];
+        });
+    }
 };
 
 mainFn();
@@ -40,25 +40,5 @@ mainFn();
 
 
 window.addEventListener("resize", () => {
-  window.location.reload();
+    window.location.reload();
 });
-
-
-let vasya = { name: "Вася", surname: "Пупкин", id: 1 };
-let petya = { name: "Петя", surname: "Иванов", id: 2 };
-let masha = { name: "Маша", surname: "Петрова", id: 3 };
-
-let users = [ vasya, petya, masha ];
-
-let usersMapped = /* ... ваш код ... */
-
-/*
-usersMapped = [
-  { fullName: "Вася Пупкин", id: 1 },
-  { fullName: "Петя Иванов", id: 2 },
-  { fullName: "Маша Петрова", id: 3 }
-]
-*/
-
-alert( usersMapped[0].id ) // 1
-alert( usersMapped[0].fullName ) // Вася Пупкин
